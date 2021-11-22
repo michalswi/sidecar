30148425197d53e16ca5234745f53a497e51eb1f
ms <michal.swierczewski@nokia.com>
2021-11-22 10:04:32 +0100

    containers+k8s

--- proxy.go
+++ proxy.go
@@ -9,23 +9,37 @@ import (
 	"os"
 )
 
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return defaultValue
+	}
+	return value
+}
+
 func main() {
-	logger := log.New(os.Stdout, "web app ", log.LstdFlags|log.Lshortfile)
+	logger := log.New(os.Stdout, "proxy ", log.LstdFlags|log.Lshortfile)
 	logger.Println("Proxy is starting...")
 
-	//
+	// Proxy Port
 	proxyPort := os.Getenv("PPORT")
 	if proxyPort == "" {
 		logger.Fatal("PPORT env var is missing.")
 	}
 
-	//
+	// Application Port
 	appPort := os.Getenv("APORT")
 	if appPort == "" {
-		logger.Fatal("PPORT env var is missing.")
+		logger.Fatal("APORT env var is missing.")
 	}
 
-	url, err := url.Parse("http://localhost:" + appPort)
+	// Application IP
+	appIP := getEnv("AIP", "localhost")
+	if appIP == "" {
+		logger.Fatal("AIP env var is missing.")
+	}
+
+	url, err := url.Parse("http://" + appIP + ":" + appPort)
 	if err != nil {
 		logger.Fatalf("Error parsing backend url: %v", err)
 	}
